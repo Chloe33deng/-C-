d7d2992e00c0e3b09fbbc2b40ca06bdc2113d603
邓珊珊07 <2904033530@qq.com>
2022-03-21 06:27:52 +0000

    用不带参数和带一个参数的构造函数给对象数组赋值

---
+++ 3.15.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class exam{
+	public:
+		exam()
+		{
+			x=123;
+		}
+		exam(int n)
+		{
+			x=n;
+		}
+		int get_x()
+		{
+			return x;
+		}
+	private:
+		int x;
+};
+int main()
+{
+	exam ob1[4]={11,22,33,44};
+	exam ob2[4]={55,66};
+	for (int i=0;i<4;i++)
+	cout<<ob1[i].get_x()<<' ';
+	cout<<endl;
+	for (int i=0;i<4;i++)
+	cout<<ob2[i].get_x()<<' ';
+	return 0;
+} 
